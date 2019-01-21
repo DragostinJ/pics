d5beece7ff079c456f67c39ab69379962f119cd3
unknown <dragostinj@gmail.com>
2019-01-21 13:33:40 +0200

    Updates

---
+++ precache-manifest.331406db74640e3fddc15be98c3cd93f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e0d4f6ad25f02f07e2d",
+    "url": "/pics/static/js/runtime~main.9e0d4f6a.js"
+  },
+  {
+    "revision": "378be96c73f60bf9a5f5",
+    "url": "/pics/static/js/main.378be96c.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/pics/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "378be96c73f60bf9a5f5",
+    "url": "/pics/static/css/main.180332b9.chunk.css"
+  },
+  {
+    "revision": "9bd67afe7b5b9c3839da24e3e2a718f5",
+    "url": "/pics/index.html"
+  }
+];
